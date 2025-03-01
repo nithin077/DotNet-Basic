57b3c9c9452d0a12e5c833760e8a6d780cf3715b
nithin077 <nithinchinn@gmail.com>
2025-03-02 00:32:56 +0530

    Validation for API's added

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Product.Models;
@@ -10,15 +11,44 @@ namespace Product.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Student> GetStudentDetails()
+        [Route("GetAllStudents")]
+        public ActionResult<IEnumerable<Student>> GetStudentDetails()
         {
             return CollegeRepository.Student;
         }
 
-        [HttpGet("{id}")]
-        public Student GetStudentDetailsById(int id)
+        [HttpGet]
+        [Route("GetStudentById/{id:int}")]
+        public ActionResult<Student> GetStudentDetailsById(int id)
         {
-            return CollegeRepository.Student.Where(col => col.id == id).FirstOrDefault();
+            if (id <= 0) return BadRequest("Student id is required");
+            var student = CollegeRepository.Student.Where(col => col.id == id).FirstOrDefault();
+            if (student == null) return NotFound($"Student With id : {id} Not Found");
+            return Ok(student);
+        } 
+
+        [HttpGet]
+        [Route("GetStudentByName/{name}")]
+        public ActionResult<Student> GetStudentDetailsByName(string name)
+        {
+            if (string.IsNullOrEmpty(name)) return BadRequest("Student Name required");
+            var result = CollegeRepository.Student.Where(col => col.name == name).FirstOrDefault();
+            if (result == null) return NotFound($"Student With name : {name} Not Found");
+            return result;
+
+        }
+
+        [HttpDelete]
+        [Route("DeleteStudentById/{id:int}")]
+
+        public ActionResult<bool> DeletStudentById(int  id)
+        {
+            if (id <= 0) return BadRequest("Student Id is required should not be 0");
+            var student = CollegeRepository.Student.Where(col => col.id == id).FirstOrDefault();
+            if (student == null) return NotFound($"Student With id : {id} Not Found");
+            CollegeRepository.Student.Remove(student);
+            return true;
+
         }
     }
 }
